ef838e058b12a080184bfd69e99654334bf8a134
utkarsh <utkarshp@cdac.in>
2025-12-10 17:47:35 +0530

    upd

--- src/main/java/com/solid/quick_shop/ds/DsExe.java
+++ src/main/java/com/solid/quick_shop/ds/DsExe.java
@@ -7,23 +7,8 @@ import java.util.Set;
 
 public class DsExe {
 
+    //
     public static boolean containsDuplicate(int[] nums) {
-        int n = nums.length;
-
-        // Using  hashmap : 1
-        // Map<Integer, Integer> map = new HashMap<>();
-
-        // for (int i : nums) {
-        //     if (map.containsKey(i)) {
-        //         return true;
-        //     } else {
-        //         map.put(i, 1);
-        //     }
-        // }
-
-        // return false;
-
-        //Using HashSet
         Set<Integer> set = new HashSet<>();
         for (int i : nums) {
             if (set.contains(i)) {
@@ -34,9 +19,67 @@ public class DsExe {
         return false;
     }
 
+     public static boolean isAnagram(String s, String t) {
+        int len = s.length();
+        int len2 = t.length();
+        if(len!=len2) return false;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0; i <len;i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }else{
+                map.put(s.charAt(i), 1);
+            }
+        }
+        for (int i =0; i<len2;i++){
+            if(map.containsKey(t.charAt(i))){
+                map.put(t.charAt(i), map.get(t.charAt(i))-1);
+            }else{
+                return false;
+            }
+        }
+        for(Map.Entry<Character,Integer> etr: map.entrySet()){
+            if (etr.getValue()!=0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        // for(int i=0;i<n;i++){
+        //     System.out.println(map);
+        //     map.put(nums[i], i);
+            
+        // }
+
+        for(int i=0;i<n;i++){
+            System.out.println(map);
+            if(map.containsKey(target-nums[i])){
+                return new int[]{ map.get(target-nums[i]),i};
+            }
+            map.put(nums[i], i);
+            
+        }
+
+        return new int[2];
+    }
+
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,1};
-        System.out.println(containsDuplicate(nums));
+        // int [] nums = {1,2,3,4,1};
+        // System.out.println(containsDuplicate(nums));
+
+        // String s = "aacc";
+        // String t = "ccac";
+        // System.out.println(isAnagram(s, t));
+        int [] nums = {2,7,11,15};
+        int target =9;
+        int []ans = twoSum(nums, target);
+        System.out.println(ans[0]+":"+ans[1]);
     }
 
 }
